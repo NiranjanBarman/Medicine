6e100c3a101c5d5295cc59ff87507c0a2082f30c
Protik Sarkar <sarkarprotik240@gmail.com>
2025-10-17 22:29:32 +0530

    fix data get file

--- src/App.jsx
+++ src/App.jsx
@@ -49,13 +49,12 @@ function App() {
 
   useEffect(() => {
 
-    console.log("App 1");
-
-
     const handleMessage = (event) => {
       console.log("Parent Data: ", event);
 
       if (event.origin !== "http://localhost:5173") return;
+
+
 
       if (event.data.type === "SET_USER") {
         console.log("Data from parent:", event.data.payload);
@@ -66,6 +65,9 @@ function App() {
     window.addEventListener("message", handleMessage);
     return () => window.removeEventListener("message", handleMessage);
   }, []);
+
+  window.parent.postMessage({ type: "CHILD_READY", payload: "ok" }, "http://localhost:5173");
+
 
   return (
     <Layout>
